d6cd1e41970c7677a71eef9452451c35829f1648
RaiS4N2s <me.santosrai@gmail.com>
2020-08-19 10:29:59 +0900

    update model

--- src/models/index.js
+++ src/models/index.js
@@ -1,28 +1,28 @@
 let users = {
   1: {
-    id: '1',
-    username: 'Robin Wieruch',
+    id: "1",
+    username: "Santosh Rai"
   },
   2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
+    id: "2",
+    username: "Aliza Bista"
+  }
 };
 
 let messages = {
   1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
+    id: "1",
+    text: "Lets do this",
+    userId: "1"
   },
   2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
+    id: "2",
+    text: "Lets understand",
+    userId: "2"
+  }
 };
 
 export default {
   users,
-  messages,
+  messages
 };
